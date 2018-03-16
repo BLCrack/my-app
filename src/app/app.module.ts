9b9d272ae189404d612956db4cc95b20838ce8dc
BLCrack <quaresmaaa10@wp.pl>
2018-03-16 14:35:33 +0100

    Add home, devices and google-maps components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { AdminPanelComponent } from './admin-panel/admin-panel.component';
+import { GoogleMapsComponent } from './google-maps/google-maps.component';
+import { DevicesComponent } from './devices/devices.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    AdminPanelComponent
+    AdminPanelComponent,
+    GoogleMapsComponent,
+    DevicesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
