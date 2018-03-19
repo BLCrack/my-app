60f77e540a14a0fb9b9b5e5e00585ba4ca15b9e3
xLatency <xLatency@wp.pl>
2018-03-19 16:17:32 +0100

    routing changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,21 @@ import {RouterModule, Routes} from '@angular/router';
 import {GoogleMapsComponent} from './google-maps/google-maps.component';
 import {HomeComponent} from './home/home.component';
 import {DevicesComponent} from './devices/devices.component';
+import {LoginComponent} from './login/login.component';
+import {AdminPanelComponent} from './admin-panel/admin-panel.component';
 
 const routes: Routes = [
   {path: 'map', component: GoogleMapsComponent },
   {path: 'home', component: HomeComponent},
-  {path: 'devices', component: DevicesComponent}
+  {path: 'devices', component: DevicesComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'admin', component: AdminPanelComponent},
+  {path: '', redirectTo: '/login', pathMatch: 'full'}  
 ];
 
 @NgModule({
   exports: [RouterModule],
   imports: [RouterModule.forRoot(routes)],
 })
-
-
 
 export class AppRoutingModule { }
