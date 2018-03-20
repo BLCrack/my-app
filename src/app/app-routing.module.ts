ed5b8f4bad868e3d0e7503e6495c129338281829
xLatency <xLatency@wp.pl>
2018-03-20 10:57:28 +0100

    child routing implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {GoogleMapsComponent} from './google-maps/google-maps.component';
-import {HomeComponent} from './home/home.component';
-import {DevicesComponent} from './devices/devices.component';
-import {LoginComponent} from './login/login.component';
-import {AdminPanelComponent} from './admin-panel/admin-panel.component';
+import { RouterModule, Routes } from '@angular/router';
+import { GoogleMapsComponent } from './google-maps/google-maps.component';
+import { HomeComponent } from './home/home.component';
+import { DevicesComponent } from './devices/devices.component';
+import { LoginComponent } from './login/login.component';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 
 const routes: Routes = [
-  {path: 'map', component: GoogleMapsComponent },
-  {path: 'home', component: HomeComponent},
-  {path: 'devices', component: DevicesComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'admin', component: AdminPanelComponent},
-  {path: '', redirectTo: '/login', pathMatch: 'full'}  
+
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'admin',
+    component: AdminPanelComponent,
+    children: [
+      { path: 'map', component: GoogleMapsComponent },
+      { path: 'home', component: HomeComponent },
+      { path: 'devices', component: DevicesComponent },
+      { path: '', redirectTo: 'home', pathMatch: 'full' }
+    ]
+  },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
 
 @NgModule({
