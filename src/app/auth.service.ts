73e0e8800a2230a6385a9c8fe7de3d7e3a379d15
xLatency <xLatency@wp.pl>
2018-05-23 11:51:32 +0200

    change username in token

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -9,7 +9,7 @@ export class AuthService {
   public isAuthenticated(): boolean {    
     const token = this.jwtHelper.tokenGetter();
     if (token != null) {
-      var user = this.jwtHelper.decodeToken(token).user;
+      var user = this.jwtHelper.decodeToken(token).sub;
       return user != null;
     }
     return false;
